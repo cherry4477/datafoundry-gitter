e14e4f274417dfddec8ba59bfa4c8c721808663a
yaxinlx <liuxu@asiainfo.com>
2017-03-13 17:30:23 +0800

    parse redis related envs and connect redis

--- storage.go
+++ storage.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"time"
 	"github.com/zonesan/clog"
 	"golang.org/x/oauth2"
+	"github.com/garyburd/redigo/redis"
 )
 
 var labStore = make(map[string]*oauth2.Token)
@@ -16,10 +19,72 @@ type Storage interface {
 }
 
 type RedisStore struct {
+	pool    *redis.Pool
 }
 
-func NewRedisStorage() *RedisStore {
-	return &RedisStore{}
+// addr format is host:port.
+// clusterName is blank means addr is the master address, otherwise addr is sentinel address.
+func NewRedisStorage(addr, clusterName, password string) *RedisStore {
+	var p = &redis.Pool{
+		MaxIdle:     10,
+		MaxActive:   10,
+		Wait:        true,
+		IdleTimeout: 4 * time.Minute,
+		Dial: func() (redis.Conn, error) {
+			var masterAddr string
+			if clusterName == "" {
+				masterAddr = addr
+			} else {
+				err := func() error {
+					conn, err := redis.DialTimeout("tcp", addr, time.Second*10, time.Second*10, time.Second*10)
+					if err != nil {
+						clog.Errorf("dial redis sentinel (%s) error: %s", addr, err)
+						return err
+					}
+					defer conn.Close()
+
+					redisMasterPair, err := redis.Strings(conn.Do("SENTINEL", "get-master-addr-by-name", clusterName))
+					if err != nil {
+						clog.Error("redis sentinel get-master-addr-by-name error:", err)
+						return err
+					}
+
+					if len(redisMasterPair) != 2 {
+						clog.Errorf("redis sentinel get-master-addr-by-name result invalid: %v", redisMasterPair)
+						return fmt.Errorf("redis sentinel get-master-addr-by-name result invalid: %v", redisMasterPair)
+					}
+
+					masterAddr = redisMasterPair[0] + ":" + redisMasterPair[1]
+					return nil
+				}()
+				
+				if err != nil {
+					return nil, err
+				}
+			}
+
+			// dial master
+			c, err := redis.Dial("tcp", masterAddr)
+			if err != nil {
+				clog.Errorf("dial redis master (%s) error: %s", addr, err)
+				return nil, err
+			}
+			if password != "" {
+				if _, err := c.Do("AUTH", password); err != nil {
+					clog.Errorf("redis master (%s) AUTH error: %s", addr, err)
+					c.Close()
+					return nil, err
+				}
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+	
+	return &RedisStore{pool: p}
 }
 
 func (rs *RedisStore) LoadTokenGitlab(user string) *oauth2.Token {
